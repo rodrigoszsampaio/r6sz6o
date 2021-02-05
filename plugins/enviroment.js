236eb5d8848093d8aabc20f0e9ec46373bb65f34
Rodrigo <rodrigo.souza@ibm.com>
2021-02-04 21:30:11 -0300

    aeeeee

--- plugins/enviroment.js
+++ plugins/enviroment.js
@@ -3,8 +3,9 @@ const fp = require('fastify-plugin')
 async function enviroment (fastify) {
   const schema = {
     type: 'object',
-    required: [ 'MONGODB_URL', 'JWT_SECRET', 'TOKEN_EXPIRES_IN' ],
+    required: [ 'MONGODB_URL', 'JWT_SECRET', 'TOKEN_EXPIRES_IN', 'PORT' ],
     properties: {
+      PORT: { type: 'number' },
       MONGODB_URL: { type: 'string' },
       JWT_SECRET: { type: 'string' },
       TOKEN_EXPIRES_IN: { type: 'string' },
